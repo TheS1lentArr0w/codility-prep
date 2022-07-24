e7b1b3f7ba831d88b3882bee095875e92ec14f83
TheS1lentArr0w <paulocruz0123@hotmail.com>
2022-07-25 00:24:37 +0100

    stuck on countdiv

--- Lesson_5/passingcars.py
+++ Lesson_5/passingcars.py
@@ -1,2 +1,24 @@
+#######
+## Really elegant solution
+## Link: https://stackoverflow.com/questions/48046020/passingcars-in-codility-using-python
+## Time complexity: O(N)
+#######
+
 def solution(A):
+    zeros = 0
+    passing = 0
+
+    for elem in A:
+        if elem == 0:
+            # keep totalling zeros
+            zeros += 1
+        else:
+            # every time a 1 occurs, all zeros
+            ## pass it
+            passing += zeros
     
+    # edge case
+    if passing > 1000000000:
+        return -1
+    
+    return passing
